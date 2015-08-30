522555921013676a13f7c0b2c76a8d3a2f0b54ed
JTR4BD <jtr4bd@mail.missouri.edu>
2015-08-30 17:32:59 -0500

    Create jtr4bd.c

---
+++ sentences/jtr4bd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int jtr4bdSentence1(){
+	printf("he slayed that weave and had that mug laid\n");
+	return 1;
+}
+
+int jtr4bdSentence2(){
+	printf("so my ninja had no more fruit\n");
+	return 1;
+}
